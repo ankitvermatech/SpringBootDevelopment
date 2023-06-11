8392081d2fdf4c4379e0b74cebdb6842438f30b8
Ankit Verma <ankitepsverma@gmail.com>
2023-06-11 07:37:44 +0530

    Learning @GetMapping

---
+++ SpringBoot/springBootApp/src/main/java/com/verma/springboot/learning/FirstClass.java
@@ -0,0 +1,25 @@
+package com.verma.springboot.learning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstClass {
+
+    @GetMapping(path = "/first-class")
+    public String firstMethod(){
+        return "Hello";
+    }
+
+    @GetMapping(path = "/first-class-bean")
+    public FirstClassBean firstClassBean(){
+        return new FirstClassBean("Hello Ankit");
+    }
+
+    @GetMapping(path = "/first-class/student/{name}")
+    public FirstClassBean studentName(@PathVariable String name){
+        return new FirstClassBean(String.format("Hello %s",name));
+    }
+
+}
