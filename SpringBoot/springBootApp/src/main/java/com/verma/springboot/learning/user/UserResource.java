8afb6adac6bdfd3fec6dfc4a1ae7dab48d136822
Ankit Verma <ankitepsverma@gmail.com>
2023-06-20 23:37:44 +0530

    Implemented HATEOAS in Spring Boot application

--- SpringBoot/springBootApp/src/main/java/com/verma/springboot/learning/user/UserResource.java
+++ SpringBoot/springBootApp/src/main/java/com/verma/springboot/learning/user/UserResource.java
@@ -2,6 +2,8 @@ package com.verma.springboot.learning.user;
 
 import com.verma.springboot.learning.user.userDefinedException.UserNotFoundException;
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -24,12 +26,15 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
         if(user==null){
             throw new UserNotFoundException("Id: "+id);
         }
-        return user;
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+        return entityModel;
     }
 
     @PostMapping("/users")
